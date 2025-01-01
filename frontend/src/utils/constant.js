10d0017abb0b96b2e4939d9f888d2973e161bf46
Shaik Yaseen <hackercanhack69@gmail.com>
2025-01-01 15:18:12 +0530

    deployment changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,8 @@
 export const USER_API_END_POINT =
-  "https://jobportal-yy6w.onrender.com/api/v1/user";
+  "https://jobportal-1btx.onrender.com/api/v1/user";
 export const JOB_API_END_POINT =
-  "https://jobportal-yy6w.onrender.com/api/v1/job";
+  "https://jobportal-1btx.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://jobportal-yy6w.onrender.com/api/v1/application";
+  "https://jobportal-1btx.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://jobportal-yy6w.onrender.com/api/v1/company";
+  "https://jobportal-1btx.onrender.com/api/v1/company";
